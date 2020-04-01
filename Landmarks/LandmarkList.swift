ce1695c4c377e2f0ed923e9e98d8e7c0f131e9ef
Paulo Orquillo <paulo.orquillo@davanti.co.nz>
2020-04-02 00:30:37 +1300

    environmentobject basics

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -9,16 +9,16 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State var showFavoritesOnly = false
+    @EnvironmentObject var userData: UserData
     
     var body: some View {
         NavigationView {
             List{
-                Toggle(isOn: $showFavoritesOnly) {
+                Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
                 }
-            ForEach(landmarkData) { landmark in
-                if !self.showFavoritesOnly || landmark.isFavorite {
+                ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
                 NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
                 }
@@ -36,5 +36,7 @@ struct LandmarkList_Previews: PreviewProvider {
             .previewDevice(PreviewDevice(rawValue: deviceName))
             .previewDisplayName(deviceName)
         }
+       .environmentObject(UserData())
+
     }
 }
