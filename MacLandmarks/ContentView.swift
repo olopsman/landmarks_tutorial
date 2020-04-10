358400e0a3f73e6bdd6b8669cc57fe8404d5c1e0
Paulo Orquillo <paulo.orquillo@davanti.co.nz>
2020-04-10 17:05:03 +1200

    applewatch support

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Paulo Orquillo on 10/04/20.
+//  Copyright © 2020 Quonsepto Limited. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
